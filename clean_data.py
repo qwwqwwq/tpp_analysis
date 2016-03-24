2158f54974c522da562b12bb41a3533b90974bcf
qwwqwwq <jeffreyquinn@Jeffreys-MacBook-Air.local>
2016-03-23 22:33:08 -0700

    produce reasonable json struct from data

--- clean_data.py
+++ clean_data.py
@@ -18,21 +18,21 @@ COUNTRIES = [
 
 def read_code_mappings():
     with open("US_code_map.json") as f:
-	return json.load(f)
+        return json.load(f)
 
 def parse_value(s):
     if is_percent(s):
-	return float(s.strip().strip('%')), '%'
+        return float(s.strip().strip('%')), '%'
     if len(s.split()) == 1 and '/' in s:
-	scalar, units = s.split('/', 1)
-	units = '/' + units
+        scalar, units = s.split('/', 1)
+        units = '/' + units
     else:
-	scalar, units = s.split(None, 1)
+        scalar, units = s.split(None, 1)
     scalar = scalar.strip()
     # $1 /1000 -> 1 $ /1000
     if scalar.startswith('$'):
-	units = '$ ' + units
-	scalar = scalar.strip('$')
+        units = '$ ' + units
+        scalar = scalar.strip('$')
     return float(scalar), units
 
 def is_percent(s):
@@ -40,31 +40,34 @@ def is_percent(s):
 
 def us_base_rate_modification(base_rate, modification):
     if base_rate.strip() == 'Free':
-	return '0.0%'
+        return '0.0%'
     if ModificationTypes.PROPORTION in modification:
-	scalar, units = parse_value(base_rate)
-	return str(scalar * modification[ModificationTypes.PROPORTION]) + units
+        try:
+            scalar, units = parse_value(base_rate)
+            return str(scalar * modification[ModificationTypes.PROPORTION]) + units
+        except:
+            return numpy.nan
     elif ModificationTypes.AD_VALOREM in modification:
-	return str(modification[ModificationTypes.AD_VALOREM]) + '%'
+        return str(modification[ModificationTypes.AD_VALOREM]) + '%'
 
 def read_schedule():
     return pandas.read_csv("TPP-Final-Text-US-Tariff-Elimination-Schedule.csv")
 
-def process_row(year, code_map, row):
+def process_row(code_map, row):
     for country in COUNTRIES:
-	if row[country] in code_map:
-	    row[country] = us_base_rate_modification(row['Base Rate'], code_map[row[country]][year])
-	else:
-	    row[country] = numpy.nan
+        if row[country] in code_map:
+            row[country] = [us_base_rate_modification(row['Base Rate'], code_map[row[country]][year]) 
+                for year in range(30) if len(code_map[row[country]]) > year]
+        else:
+            row[country] = numpy.nan
     return row
 
-def process_year(schedule, code_map, year):
-    return (schedule.apply(functools.partial(process_row, year, code_map), axis=1))
+def process_year(schedule, code_map):
+    return (schedule.apply(functools.partial(process_row, code_map), axis=1))
 
-def process_years(schedule, code_map):
-    for i in range(30):
-	df = process_year(read_schedule(), read_code_mappings(), i)
-	import ipdb; ipdb.set_trace()
+def write_json_blob(schedule, code_map):
+    with open("blob.json", "w") as of:
+        of.write(process_year(read_schedule(), read_code_mappings()).to_json(orient="records"))
 
 if __name__ == '__main__':
-    df = process_years(read_schedule(), read_code_mappings())
+    write_json_blob(read_schedule(), read_code_mappings())
